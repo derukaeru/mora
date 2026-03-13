f25222b0fbdaa52a4269aeadcbc4477882c43dc1
dalifyyy <d4lified@gmail.com>
2026-03-13 10:57:08 +0800

    .

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+func _ready():
+	$ui/cover.show()
+	$ui/cover/AnimationPlayer.play("open")
+	await $ui/cover/AnimationPlayer.animation_finished
+	$ui/cover.hide()
--- scenes/objects/player/player.gd
+++ scenes/objects/player/player.gd
@@ -1,25 +1,40 @@
 extends CharacterBody2D
 
-
-const SPEED = 80.0
-const JUMP_VELOCITY = -250.0
-
+const DELAY = 2.0  # seconds
+var input_queue = []
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	var time_now = Time.get_ticks_msec() / 1000.0
+	
+	var input = {
+		"time": time_now,
+		"jump": Input.is_action_just_pressed("jump"),
+		"left": Input.is_action_pressed("left"),
+		"right": Input.is_action_pressed("right")
+	}
+	input_queue.append(input)
+	
+	var to_remove = []
+	for entry in input_queue:
+		if time_now - entry["time"] >= DELAY:
+			execute_input(entry)
+			to_remove.append(entry)
+	
+	for entry in to_remove:
+		input_queue.erase(entry)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func execute_input(entry):
+	if entry["jump"] and is_on_floor():
+		velocity.y = -400
+	
+	var direction = 0
+	if entry["left"]:
+		direction -= 1
+	if entry["right"]:
+		direction += 1
+	velocity.x = direction * 200
+	
 	move_and_slide()
